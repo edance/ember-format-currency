355d4b1c3613e3857ddc4f959982b6de3261541f
Evan Dancer <evancdancer@gmail.com>
2016-07-20 13:33:05 -0400

    Update to ember 2.6

--- addon/helpers/format-currency.js
+++ addon/helpers/format-currency.js
@@ -1,12 +1,12 @@
-import Ember from "ember";
+import Ember from 'ember';
 
 export default Ember.Helper.helper(function(params) {
   var n = params[0],
       c = isNaN(c = Math.abs(params[1])) ? 2 : c,
-      d = ".",
-      t = ",",
-      i = parseInt(n = Math.abs(+n || 0).toFixed(c)) + "",
+      d = '.',
+      t = ',',
+      i = parseInt(n = Math.abs(+n || 0).toFixed(c)) + '',
       j = (j = i.length) > 3 ? j % 3 : 0;
 
-  return "$" + (j ? i.substr(0, j) + t : "") + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + t) + (c ? d + Math.abs(n - i).toFixed(c).slice(2) : "");
+  return '$' + (j ? i.substr(0, j) + t : '') + i.substr(j).replace(/(\d{3})(?=\d)/g, '$1' + t) + (c ? d + Math.abs(n - i).toFixed(c).slice(2) : '');
 });
